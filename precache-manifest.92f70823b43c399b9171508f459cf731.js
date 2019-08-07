135b3ca7be8500d42d5776071b1d85747a5b3b4f
HellooCK <46767604+HellooCK@users.noreply.github.com>
2019-08-07 12:53:28 +1000

    Updates

---
+++ precache-manifest.92f70823b43c399b9171508f459cf731.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af998ef9692a2d54fc9324fb873cfd77",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "1010972c1827aa09c6e9",
+    "url": "/Monsters-Rolodex/static/css/main.dd48b04c.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/Monsters-Rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "1010972c1827aa09c6e9",
+    "url": "/Monsters-Rolodex/static/js/main.a625288d.chunk.js"
+  },
+  {
+    "revision": "a3aeee1dcfe197e62d5f",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.650339cb.js"
+  }
+]);
